e04b172bed8fc4ca39d406bb6812562d53979893
phamhac178 <phamhac178@gmail.com>
2022-11-26 12:25:40 +0700

    first commit

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.connect("mongodb+srv://new-user1:phamhac90@cluster0.flrto.mongodb.net/final?retryWrites=true&w=majority",{useNewUrlParser: true});
+        await mongoose.connect("mongodb+srv://new-user1:phamhac90@cluster0.flrto.mongodb.net/finalgit push -u origin main?retryWrites=true&w=majority",{useNewUrlParser: true});
     } catch (error) {
         console.log(error); 
     }
